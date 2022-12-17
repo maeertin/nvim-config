135a691e6c95e2b1922de560033bdee34dd8ef15
Martin Barri <maeertin@gmail.com>
2022-12-17 02:06:13 +0100

    update to new folder structure

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,31 +4,24 @@ if not status_ok then
 end
 
 configs.setup({
+	-- A list of parser names, or "all"
 	ensure_installed = {
 		"css",
-		"dockerfile",
-		"fish",
-		"git_rebase",
-		"gitattributes",
-		"gitignore",
-		"glsl",
-		"go",
-		"graphql",
 		"html",
-		"http",
 		"javascript",
 		"json",
 		"lua",
-		"php",
-		"python",
 		"rust",
-		"scss",
-		"sql",
 		"tsx",
 		"typescript",
-		"yaml",
 	},
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 	highlight = {
+		-- `false` will disable the whole extension
 		enable = true,
 	},
 	autopairs = {
--- init.lua
+++ init.lua
@@ -1,3 +1,1 @@
-require("options")
-require("keymaps")
-require("packer-plugins")
+require("user")
